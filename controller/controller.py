9f505c5a13bee9d0b380f5378352f68d22906d27
Alan Sparrow <git@freeflight.org.uk>
2020-01-04 21:11:44 +0000

    Add switch modes

--- controller/controller.py
+++ controller/controller.py
@@ -10,6 +10,9 @@ from .suntime import Sun
 LAT = 51.0
 LON = -1.6
 
+SWITCH_ON = 1
+SWITCH_OFF = -1
+
 class Timer:
     def __init__(self, delta):
         self.delta = delta
@@ -45,13 +48,24 @@ class DailyTimer(Timer):
 
 class Controller:
     def __init__(self, switches, resolution=60):
-        self.switches = {s[0]: {'mode': 'off', 'timers': {}, 'value': s[1]} for s in switches}
+        self.switches = {s[0]: {'mode': 'manual', 'timers': {}, 'value': s[1]} for s in switches}
         self.resolution = resolution
 
+        self.secs = int(time.time())
         self.timer_id = 0
 
     def set_switch(self, switch, mode):
         self.switches[switch]['mode'] = mode
+        if mode == 'manual':
+            return
+        elif mode == 'on':
+            val = self.switches[switch]['value']
+        elif mode == 'auto':
+            val = self._get_auto_value(switch, self.secs)
+        else:
+            val = 0
+
+        self._set_switch(switch, val)
 
     def add_timer(self, timer, switch):
         self.timer_id += 1
@@ -74,17 +88,29 @@ class Controller:
 
     # _update is called every self.resolution seconds
     def _update(self, secs):
-        for s in self.switches:
-            acc = 0
-            for t in self.switches[s]['timers'].values():
-                if t <= secs:
-                    acc += t.delta
+        for s, switch in self.switches.items():
+            if switch['mode'] == 'manual':
+                continue
+            elif switch['mode'] == 'on':
+                value = switch['value']
+            elif switch['mode'] == 'off':
+                value = 0
+            else:
+                value = self._get_auto_value(s, secs)
 
-            value =  self.switches[s]['value'] if acc > 0 else 0
-            print(datetime.now(), s, value)
             self._set_switch(s, value)
 
+    def _get_auto_value(self, switch, secs):
+        acc = 0
+        for t in self.switches[switch]['timers'].values():
+            if t <= secs:
+                acc += t.delta
+
+        val =  self.switches[switch]['value'] if acc > 0 else 0
+        return val
+
     def _set_switch(self, switch, value):
+        print(datetime.now(), switch, value)
         url = "http://homeweb:5000/api/switch/%s" % switch
         data = json.dumps(value).encode('utf-8')
         req = urllib.request.Request(url=url, data=data, method='PUT',
